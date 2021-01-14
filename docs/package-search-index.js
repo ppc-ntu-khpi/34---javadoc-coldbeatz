ceec563b496295fe245b3a8920cf5c85c724ee8d
coldbeatz <59085332+coldbeatz@users.noreply.github.com>
2021-01-14 14:20:11 +0200

    Add files via upload
    javadoc final

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"domain"},{"l":"test"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"domain"}]
